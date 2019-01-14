a5a5f05f7f4514caf64b5e91a5726a8c5651bbd2
MoTraore <moussatd132@gmail.com>
2019-01-14 09:49:44 +0100

    Incrusión de bucle

--- src/HelloWorldGit1/HelloWorld.java
+++ src/HelloWorldGit1/HelloWorld.java
@@ -5,8 +5,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hello World.");
 		System.out.print("Esto es para compartir");
-		System.out.print("Esto es para compartir");
+		System.out.print("Mas codigo");
 
+	for (int i=0;i<10;i++) {
+		System.out.print("hola");
+	}
 	}
 
 }
