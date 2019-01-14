850665ab752bbf53435ce461bfd319586ad64063
MoTraore <moussatd132@gmail.com>
2019-01-14 09:37:30 +0100

    Se ha seguido el trabajo

--- src/HelloWorldGit1/HelloWorld.java
+++ src/HelloWorldGit1/HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.print("Hello World.");
+		System.out.print("Esto es para compartir");
+		System.out.print("Esto es para compartir");
 
 	}
 
